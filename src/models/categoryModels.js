8692662965a250c9d23ddab28e481f8393bd1bec
FlorenciaCampos <florenciacampos460@gmail.com>
2025-09-28 12:05:59 -0300

    se crea categoryModel

---
+++ src/models/categoryModels.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        maxlength: 30,
+        minlength: 2
+    }
+}, {
+    timestamps: true} ) //se agregan dos campos: createdAt: fecha en que se creo el doc// updateAt: en que se modifica
+
+    export default mongoose.model("category", categorySchema) //"category",lo usamos como referencia en products
