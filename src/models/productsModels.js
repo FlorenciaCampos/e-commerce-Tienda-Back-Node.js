c00d50bfcd6faebacb35bcdaa64edb1d58b04dd8
FlorenciaCampos <florenciacampos460@gmail.com>
2025-09-28 12:39:22 -0300

    se borran comentarios

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -32,22 +32,17 @@ const productSchema = new mongoose.Schema({
         minLength: 5,
         maxLength: 200,
     },
-    // El status lo manejamos con un ENUM de posibles valores
+   
     status: {
         type: String,
-        // El campo validate sirve para generar una funcion de validacion para nuestro campo
             validate: {
-                // Validator contiene una funcion que permite validar algo del campo
-                // status es el campo
                 validator: function (status) {
                     return statusEnum.includes(status)
                 },
-                // props es el dato que nos llegó en status
                 message: props => `${props.value} it's not a valid status`
             }
     },
-    // Esta seria el equivalente a FK en bases de datos noSQL
-    // Ref es a que modelo se refiere, y cual modelo/collection es dueño de ese ObjectId
+   
     category: {type: mongoose.Schema.Types.ObjectId, ref: "category" },
 
     stock: {
@@ -56,17 +51,13 @@ const productSchema = new mongoose.Schema({
         min: [0, "Stock can't be a negative number"]
     },
 
-    // Campo de destacados
+
     highlighted: {
         type: Boolean,
         default: false
     },
 })
 
-    // Metodos de instancia para disminuir el stock
-    // amount es la cantidad "vendida" que se resta al stock
-    // Este metodo queda guardado en el modelo y cuando lo necesitamos lo usamos
-    // No se ejecuta automaticamente, entonces lo incluimos donde necesitamos restar stock
     productSchema.methods.decreaseStock = async function (amount) {
         if(amount <= 0){
             throw new Error("Amount has to be a positive value")
@@ -75,22 +66,15 @@ const productSchema = new mongoose.Schema({
         if(this.stock < amount) {
             throw new Error("Not enough quantity")
         }
-        // stock = stock - amount
         this.stock -= amount
-        // Se guarda en la db el nuevo valor
         await this.save()
     }
 
-    // Atributos/propiedades virtuales, sirven para:
-    // Calcular el precio con la tasa de ganancia
-    // Permite generar un valor sin haberlo escrito en el esquema
-    // Facilita mucho porque podemos hacer calculos con nuestros propios valores
-    // Datos pre calculados
+  
     productSchema.virtual("priceWithProfitRate").get(function () {
         return this.price * this.profitRate
     })
 
-    // Es una configuracion para incluir las propiedades virtuales
     productSchema.set("toJSON", {virtuals: true})
     productSchema.set("toObject", {virtuals: true})
 
